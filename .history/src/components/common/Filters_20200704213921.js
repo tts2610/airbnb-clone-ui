f835e4b519a7da59b4a2d24b8b376bffec1835a0
Sean <psytst@nottingham.ac.uk>
2020-07-04 21:49:03 +0700

    first commit

---
+++ .history/src/components/common/Filters_20200704213921.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import MultiSelect from "@khanacademy/react-multi-select";
+const options = [
+  {label: "One", value: 1},
+  {label: "Two", value: 2},
+  {label: "Three", value: 3},
+];
+export default function Filters() {
+    const [selected, setSelected] = useState([])
+    return (
+        <MultiSelect
+      options={options}
+      selected={selected}
+      onSelectedChanged={selected => setSelected(selected))}/>
+    )
+}
